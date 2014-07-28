5309e1eee36d8c512825d900a49e0daed99cb85b
forrestbthomas <forrestbthomas@gmail.com>
2014-07-28 15:54:50 -0700

    Updated package.json

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,13 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+	var body = '';
+	req.on('data', function(data){
+		body += data;
+	});
+	req.on('end', function(){
+		JSON.parse(body)
+	});
+  res.writeHead(200, archive.headers);
+  res.end(JSON.stringify(body));
 };
